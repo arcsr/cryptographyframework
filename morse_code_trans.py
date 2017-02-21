918c107672e7b267d9be2f7289cd3ac4a0e2b737
cwayment <cwayemnt@spillman.com>
2017-02-21 12:54:12 -0700

    fixed issue with array and added numbers to morse_code

--- morse_code_trans.py
+++ morse_code_trans.py
@@ -6,6 +6,8 @@ import string
 args = sys.argv[1:]
 
 lower = string.ascii_lowercase
+digits = string.digits
+common = lower + digits
 
 def main():
 
@@ -23,29 +25,31 @@ def main():
 		print "Usage morse_code_trans.py -s string -e|-d\n"	
 		sys.exit(1)
 
-	letters_morse = {}
-	letters_code = [".-","-...","-.-.","-..",".","..-.","--.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-	morse_letters = {}
-	x = 0
-	for i in lower:
-		letters_morse[letters_code[x]] = i		
-		morse_letters[i] = letters_code[x]
-		x += 1
+	print common
 
+	common_morse = {}
+	common_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----."]
+	morse_common = {}
+	x = 0
+	for i in common:
+#		print "%d %s %s" % (x, i, common_code[x]) 
+		common_morse[common_code[x]] = i		
+		morse_common[i] = common_code[x]
+		x += 1
 
 	return_string = ""
 
 	if(to_do == "decode"):
-		returned_string = decode_string(text,letters_morse)
+		returned_string = decode_string(text,common_morse)
 
 	print returned_string
 
-def decode_string(text,letters_morse):
+def decode_string(text,common_morse):
 	new_string = ""
 	split_text = text.split(" ")
 	for i in split_text:
-		if(i in letters_morse):
-			new_string += letters_morse[i]		
+		if(i in common_morse):
+			new_string += common_morse[i]		
 		else:
 			new_string += " "
 
